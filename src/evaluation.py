c691fffb68367bb24b251e34b6d1681cbe79471d
r-wata <ryusei.watanabe@fuji.waseda.jp>
2023-05-26 16:39:31 +0900

    :+1: Add evaluation.py

---
+++ src/evaluation.py
@@ -0,0 +1,24 @@
+from setfit import SetFitModel, SetFitTrainer
+from sentence_transformers.losses import CosineSimilarityLoss
+
+from training import read_dataset, compute_metrics
+
+
+if __name__ == "__main__":
+    # Load dataset
+    dataset = read_dataset("preprocessing/dataset/dataset.json")
+    validation_data = dataset["validation"].shuffle()
+
+    # Build training model
+    model = SetFitModel.from_pretrained("../tuned_models/all-MiniLM-L6-v2", local_files_only=True)
+
+    # Validate model
+    trainer = SetFitTrainer(
+        model=model,
+        train_dataset=None,
+        eval_dataset=validation_data,
+        loss_class=CosineSimilarityLoss,
+        metric=compute_metrics
+    )
+    metrics = trainer.evaluate()
+    print(metrics)
